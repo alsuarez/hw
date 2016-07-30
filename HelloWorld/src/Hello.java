b655744f05c74f4426bdb8453923516c6eff8b59
Alex <alex.suarez100@gmail.com>
2016-07-30 17:28:44 -0500

    new code

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("Let's play!");
+		System.out.println("Let's play a game!");
 	}
 
 }
