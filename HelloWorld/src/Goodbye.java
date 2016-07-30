26a83e9f79f9461cdbe794820a922aa3e10dde89
Alex <alex.suarez100@gmail.com>
2016-07-30 15:28:44 -0500

    Edited output message

--- HelloWorld/src/Goodbye.java
+++ HelloWorld/src/Goodbye.java
@@ -4,6 +4,7 @@ public class Goodbye {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("good bye!");
+		System.out.println("I said...");
 	}
 
 }
